b57689a8d0cc82abfa65320c63f9de9c68fc093e
Bonezegei <93502321+bonezegei@users.noreply.github.com>
2023-08-01 05:23:07 +0800

    Update Font.h
    Adding Bold Arial 8,10,12 fonts and Arial 16, Removing all poppins fonts

--- src/Font.h
+++ src/Font.h
@@ -5,28 +5,26 @@
 */
 #ifndef _FONT_H_
 #define _FONT_H_
-#include "fonts/Poppins8.h"
-#include "fonts/Poppins10.h"
-#include "fonts/Poppins12.h"
-#include "fonts/Poppins14.h"
-#include "fonts/Poppins16.h"
-
 #include "fonts/Arial8.h"
 #include "fonts/Arial10.h"
 #include "fonts/Arial12.h"
 #include "fonts/Arial14.h"
+#include "fonts/Arial16.h"
+
+#include "fonts/Arial8Bold.h"
+#include "fonts/Arial10Bold.h"
+#include "fonts/Arial12Bold.h"
 
 typedef enum {
-  FONT_POPPINS_8 = 0,
-  FONT_POPPINS_10,
-  FONT_POPPINS_12,
-  FONT_POPPINS_14,
-  FONT_POPPINS_16,
-  FONT_ARIAL_8,
+  FONT_ARIAL_8=0,
   FONT_ARIAL_10,
   FONT_ARIAL_12,
   FONT_ARIAL_14,
-} FONT_TYPE;
+  FONT_ARIAL_16,
+  FONT_ARIAL_8_BOLD,
+  FONT_ARIAL_10_BOLD,
+  FONT_ARIAL_12_BOLD,
+} FONT_TYPE;_TYPE;
 
 
 class FONT {
